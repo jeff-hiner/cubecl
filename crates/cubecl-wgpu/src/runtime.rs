7cce7f0b4e0728c0b967313651932dd07971ab4d
Jeff Hiner <37913568+jeff-hiner@users.noreply.github.com>
2026-02-21 23:13:56 -0700

    DEFAULT_MAX_TASKS to 512 (per testing)

--- crates/cubecl-wgpu/src/runtime.rs
+++ crates/cubecl-wgpu/src/runtime.rs
@@ -113,7 +113,7 @@ impl Default for RuntimeOptions {
         #[cfg(test)]
         const DEFAULT_MAX_TASKS: usize = 1;
         #[cfg(not(test))]
-        const DEFAULT_MAX_TASKS: usize = 32;
+        const DEFAULT_MAX_TASKS: usize = 512;
 
         let tasks_max = match std::env::var("CUBECL_WGPU_MAX_TASKS") {
             Ok(value) => value
